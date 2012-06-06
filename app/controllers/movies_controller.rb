721b739fcdb2d09d7db47980847e6bfe7df9705c
Gustavo Gomez <ggem@acm.org>
2012-06-06 16:29:27 -0600

    HW2  * Adding sorting/filtering  * Updating session with sorting/filtering options

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,4 @@
 class MoviesController < ApplicationController
-
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
@@ -7,7 +6,21 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    if update_session
+      flash.keep
+      redirect_to movies_path(params)
+      return
+    end
+    @movies = Movie.scoped
+    @movies = @movies.where(:rating => params[:ratings].keys)
+    if params[:sort_by] == 'title'
+      @movies = @movies.order('title ASC')
+      @title_class = :hilite
+    elsif params[:sort_by] == 'release'
+      @movies = @movies.order('release_date ASC')
+      @release_date_class = :hilite
+    end
+    @all_ratings = Movie.all_ratings
   end
 
   def new
@@ -38,4 +51,37 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
 
+  private
+
+  def sanitize_params
+    params.keep_if{|k,v| ["sort_by", "ratings"].include?(k) }
+  end
+  
+  def update_session
+    needs_redirect = false
+    if params[:ratings].nil?
+      needs_redirect = true
+      if (session_ratings=session[:ratings]).nil?
+        params[:ratings] = Hash[Movie.all_ratings.map{|r| [r, "1"]}]
+      else
+        params[:ratings] = session_ratings
+      end
+    else
+      session[:ratings] = params[:ratings]
+    end
+
+    if params[:sort_by].nil?
+      needs_redirect = true
+      if (session_sort_by=session[:sort_by]).nil?
+        params[:sort_by] = 'title'
+      else
+        params[:sort_by] = session_sort_by
+      end
+    else
+      session[:sort_by] = params[:sort_by]
+    end
+    
+    params.keep_if{|k, v| ["ratings", "sort_by"].include?(k) }
+    needs_redirect
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    Movie.select("distinct rating").map(&:rating)
+  end
 end
